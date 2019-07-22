e8d3559720b9ac6158dad6fe4e75cccbb0995cb3
anish adhikari <microanish@gmail.com>
2019-07-22 09:36:14 +0545

    basic issues fixed

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,37 @@
 var navbarBurger = document.getElementsByClassName('navbar-burger');
 var ul = document.getElementsByClassName('categories-ul');
 
-// navbarBurger.onclick = function() {toggleFun()};
 
-function toggleFun() {
-    ul[0].classList.toggle("open");
-    console.log(navbarBurger[0]);
+
+navbarBurger[0].onclick = () => {
+    ul[0].classList.toggle("shown");
 }
 
-// console.log(navbarBurger);
+
+
+var accImg = document.querySelector(".acc");
+var accUl = document.querySelector(".acc-ul");
+
+accImg.onclick = () => {
+    accUl.classList.toggle("open");
+}
+
+// dropdown
+var firstLi = document.querySelectorAll('.lang-curr li > ul > li');
+var langCurrDropdown = document.querySelectorAll('.lang-curr-dropdown');
+
+firstLi.forEach(function(list, index) {
+    list.onclick = () => {
+        // console.log('clicked');
+        langCurrDropdown.forEach(function(innerList, innerIndex) {
+            // innerList.setAttribute("display", "none");
+            if(index == innerIndex) {
+                console.log(innerList);
+                // innerList.style.display = 'none';
+                innerList.classList.toggle('lang-curr-dropdown-visible');
+            }
+            
+        })
+
+    }
+});
